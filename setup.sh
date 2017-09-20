3e4d434370f97ef98b48908475074e0d6ea95c18
Arsen Mamikonyan <arsen@hyelearn.com>
2017-09-20 17:51:49 +0400

    Use ubuntugis ppa
    Test Plan: make
    
    Reviewers: ashmat, daniel
    
    Reviewed By: ashmat
    
    Differential Revision: http://phabricator.hyelearn.com/D93

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-apt-get install -y \
+add-apt-repository -y ppa:ubuntugis
+
+apt-get update && apt-get install -y \
 	gfortran=4:5.3.1-1ubuntu1 \
-	libgdal-dev=1.11.3+dfsg-3build2 \
+	libgdal-dev=2.1.3+dfsg-1~xenial2 \
 	python3-dev \
 	python3-virtualenv
