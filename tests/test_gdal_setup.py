00c6804fc4e2981b16bea841375d82704d953293
Daniel <daniel@hilearn.io>
2017-09-20 19:06:17 +0400

    change requirements.txt
    Summary: add some packages
    
    Test Plan: look
    
    Reviewers: arsen
    
    Reviewed By: arsen
    
    Subscribers: ashmat
    
    Differential Revision: http://phabricator.hyelearn.com/D94

--- tests/test_gdal_setup.py
+++ tests/test_gdal_setup.py
@@ -5,3 +5,7 @@ from osgeo import gdal
 class TestGdalVersion(unittest.TestCase):
     def test_gdal_version(self):
         self.assertEquals(gdal.__version__, '2.1.3')
+
+    def test_gdal_work(self):
+        image = gdal.Open("tests/data/T38TML_20170830T074611_B01.jp2")
+        self.assertIsNotNone(image)
