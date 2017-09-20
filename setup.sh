cc990c76a45a70f7f93ad079a6d83603674f902d
Daniel <daniel@hilearn.io>
2017-09-20 16:39:54 +0400

    change readme
    Summary: change readme
    
    Test Plan: look inside
    
    Reviewers: arsen
    
    Reviewed By: arsen
    
    Subscribers: ashmat
    
    Differential Revision: http://phabricator.hyelearn.com/D91

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-apt-get install -y libgdal-dev python3-virtualenv
+apt-get install -y \
+	gfortran=4:5.3.1-1ubuntu1 \
+	libgdal-dev=1.11.3+dfsg-3build2 \
+	python3-dev \
+	python3-virtualenv
